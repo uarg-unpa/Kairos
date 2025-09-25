028f3442c5a373f48e2ef5bb7340881469660279
Gonzalo Alejandro Ulloa <gonzalo.ulloa99@gmail.com>
2025-09-25 20:20:44 -0300

    fix: proyecto creado

---
+++ kairos/src/main/java/com/nextech/kairos/KairosApplication.java
@@ -0,0 +1,13 @@
+package com.nextech.kairos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KairosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KairosApplication.class, args);
+	}
+
+}
