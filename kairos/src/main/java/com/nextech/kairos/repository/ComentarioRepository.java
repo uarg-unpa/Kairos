63de5d4e666c50ffa72ac00ad08f0a0dde2ee40a
Gonzalo Alejandro Ulloa <gonzalo.ulloa99@gmail.com>
2025-10-26 22:15:05 -0300

    feat: corregido error de id en ComentarioRepository, TareaRepository, TiempoRepository. fix: Servicio de Reporte

--- kairos/src/main/java/com/nextech/kairos/repository/ComentarioRepository.java
+++ kairos/src/main/java/com/nextech/kairos/repository/ComentarioRepository.java
@@ -7,18 +7,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import com.nextech.kairos.model.Comentario; // Asegúrate de que el paquete sea el correcto
+import com.nextech.kairos.model.Comentario;
 
 @Repository
 public interface ComentarioRepository extends JpaRepository<Comentario, Long> {
 
-    
     List<Comentario> findByTareaIdTarea(Long idTarea);
 
-    @Query("SELECT c FROM Comentario c WHERE c.usuario.idUsuario = :idUsuario")
+    @Query("SELECT c FROM Comentario c WHERE c.usuario.id = :idUsuario") 
     List<Comentario> findByUsuarioId(Long idUsuario);
 
-    @Query("SELECT c FROM Comentario c WHERE c.tarea.idTarea = :idTarea AND c.usuario.idUsuario = :idUsuario")
+    @Query("SELECT c FROM Comentario c WHERE c.tarea.idTarea = :idTarea AND c.usuario.id = :idUsuario")
     List<Comentario> findByTareaIdTareaAndUsuarioId(Long idTarea, Long idUsuario);
 
     @Query("SELECT c FROM Comentario c WHERE c.fechaComentario > :fecha")
